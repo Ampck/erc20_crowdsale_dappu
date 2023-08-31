ff22f3f9d091c088b077574025340144ffe2d583
ampck <a1j2m4@gmail.com>
2023-08-31 06:23:14 -0400

    Added Crowdsale Contract Information to Frontend

---
+++ src/components/Progress.js
@@ -0,0 +1,12 @@
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+const Progress = ({tokensSold, maxTokens}) => {
+	return(
+		<div className="my-3">
+			<ProgressBar now={((tokensSold/maxTokens) * 100)} label={`{(tokensSold/maxTokens) * 100}%`}/>
+			<p className="text-center my-3"><strong>Tokens Sold:</strong> {tokensSold} / {maxTokens}</p>
+		</div>
+	)
+}
+
+export default Progress;
